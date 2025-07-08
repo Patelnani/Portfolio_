9272d7d3a41444bc72e7518e730fdcc11f80763f
Bonagiri Anil <Bonagiri Anil@ANIL_LPT>
2025-07-08 21:16:21 +0530

    Initial commit - .NET 6 Web API

---
+++ Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace portfolioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpPost("PostFormData")]
+        public IActionResult PostFormData(Employee employee)
+        {
+            string filePath = "App_Data/employeeData.txt"; // Adjust path as needed
+
+            if (!Directory.Exists("App_Data"))
+            {
+                Directory.CreateDirectory("App_Data");
+            }
+
+            var line = $"{employee.Name}|{employee.Email}|{employee.Mobile}";
+            System.IO.File.AppendAllLines(filePath, new[] { line });
+
+            return Ok("Employee data saved successfully.");
+        }
+
+
+    }
+    public class Employee
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Mobile { get; set; }
+    }
+
+}
